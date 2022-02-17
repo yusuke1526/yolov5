3f905f5bd8b0b3b833c851f6d582f892069808a1
Yusuke Ueda <418810@m.mie-u.ac.jp>
2022-02-17 17:36:31 +0900

    add detection script

---
+++ scripts/detect/0825AMPM_combined/025_00_trim_ordinal_L2_peak1.0.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+DATASET=0825AMPM_combined
+EPOCHS=2000
+IMG_SIZE=640
+MODEL=yolov5s
+
+python train.py \
+	--data data/${DATASET}/data.yaml \
+	--cfg ${MODEL}.yaml \
+	--weights '' \
+	--batch-size 16 \
+	--epochs ${EPOCHS} \
+	--device 0 \
+	--project road_marking_deterioration \
+	--img-size ${IMG_SIZE} \
+	--name ${MODEL}_${EPOCHS}_epochs_${DATASET}_ordinal_L2_peak1.0_${IMG_SIZE}px \
+	--ordinal-cls --metric L2 \
+	--peak 1.0
